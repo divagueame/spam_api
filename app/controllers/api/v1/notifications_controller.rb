35a32bdce7a920a29659a17dbcd5be508ab9147f
Martin Arce <martin.arce.teixeira@gmail.com>
2023-03-19 11:42:40 +0100

    Clean up routes + Controller

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -1,23 +1,11 @@
 class Api::V1::NotificationsController < ApplicationController
   before_action :set_notification, only: %i[ show update destroy ]
 
-  # GET /notifications
-  def index
-    @notifications = Notification.all
-
-    render json: @notifications
-  end
-
-  # GET /notifications/1
-  def show
-    render json: @notification
-  end
-
   def create
     @notification = Notification.new(notification_params)
-
+    @notification.TypeType =  params[:Type]
     if @notification.save
-      render json: @notification, status: :created
+      render json: @notification, status: :created, location: api_v1_notifications_url(@notification)
     else
       render json: @notification.errors, status: :unprocessable_entity
     end
@@ -31,6 +19,6 @@ class Api::V1::NotificationsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def notification_params
-      params.require(:notification).permit(:RecordType, :TypeType, :TypeCode, :Name, :Tag, :MessageStream, :Description, :Email, :From, :BouncedAt)
+      params.require(:notification).permit(:RecordType, :Type, :TypeType, :TypeCode, :Name, :Tag, :MessageStream, :Description, :Email, :From, :BouncedAt)
     end
 end
